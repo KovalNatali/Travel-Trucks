dc2aec3141c73ef5ca6b12ea48fd93fea5b2ede6
KovalNatali <koval.natali.777@gmail.com>
2025-01-14 12:12:40 +0200

    Start

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Routes } from "react-router-dom";
+// import { Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <>
+      <div>Hello</div>
+      <Routes>{/* <Route path="/" element={}> */}</Routes>
+    </>
+  );
+}
