3c1d5ee03b8f68b701cdc5368888b2fe39c53e4e
KovalNatali <koval.natali.777@gmail.com>
2025-02-17 11:04:07 +0200

    add path

--- src/components/CamperItem/CamperItem.jsx
+++ src/components/CamperItem/CamperItem.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+
 import {
   addToFavorites,
   removeFromFavorites,
@@ -46,7 +47,7 @@ const CamperItem = ({ camper }) => {
                 className={css.favoriteButton}
               >
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
+                  // xmlns="http://www.w3.org/2000/svg"
                   id="favorite"
                   width="24"
                   height="24"
@@ -56,7 +57,8 @@ const CamperItem = ({ camper }) => {
                       : css.camperFavorite
                   }
                 >
-                  <use xlinkHref="/src/components/svg/symbol-defs.svg#icon-diagram" />
+                  <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                  {/* <use xlinkHref="/src/components/svg/symbol-defs.svg#icon-diagram" /> */}
                 </svg>
               </button>
             </div>
